e4323a28b147173d62213a14634e1c4a7e65998e
Ramiro Gumma <ramirogumma@hotmail.com>
2023-08-26 11:36:03 -0300

    metodos enable, disable, save fields

--- catalogo/client/gui_app.py
+++ catalogo/client/gui_app.py
@@ -30,10 +30,10 @@ class Frame(tk.Frame):
         self.root = root
         # Aca estoy usando la herencia como estoy heredando de frame directamente paso a empaquetar en vez de crear una insancia
         self.pack()
-        # self.config(bg="#001c1f")
-        self.campo_pelicula()
+        self.moovie_field()
+        self.disable_field()
 
-    def campo_pelicula(self):
+    def moovie_field(self):
         self.label_name = tk.Label(self, text="Name: ")
         self.label_name.config(font=('Arial', 12, 'bold'))
         self.label_name.grid(row=0, column=0, padx=10, pady=5)
@@ -47,22 +47,67 @@ class Frame(tk.Frame):
         self.label_gender.grid(row=3, column=0, padx=10, pady=5)
 
         # Entradas de datos
+        self.my_name = tk.StringVar()
+        self.entry_name = tk.Entry(self, textvariable=self.my_name)
+        self.entry_name.config(width=50, font=('Arial', 12))
+        self.entry_name.grid(row=0, column=1, padx=8, pady=5, columnspan=2)
 
-        self.entry_name = tk.Entry(self)
-        self.entry_name.config(width=50)
-        self.entry_name.grid(row=0, column=1, padx=8, pady=5)
+        self.my_duration = tk.StringVar()
+        self.entry_duration = tk.Entry(
+            self, textvariable=self.my_duration)
+        self.entry_duration.config(width=50, font=('Arial', 12))
+        self.entry_duration.grid(
+            row=2, column=1, padx=8, pady=5, columnspan=2)
 
-        self.entry_description = tk.Entry(self)
-        self.entry_description.config(width=50)
-        self.entry_description.grid(row=2, column=1, padx=8, pady=5)
-
-        self.entry_gender = tk.Entry(self)
-        self.entry_gender.config(width=50)
-        self.entry_gender.grid(row=3, column=1, padx=8, pady=5)
+        self.my_gender = tk.StringVar()
+        self.entry_gender = tk.Entry(self, textvariable=self.my_gender)
+        self.entry_gender.config(width=50, font=('Arial', 12))
+        self.entry_gender.grid(row=3, column=1, padx=8, pady=5, columnspan=2)
 
         # Botones
 
-        self.button_new = tk.Button(self, text="New", background="#001c1f")
+        self.button_new = tk.Button(
+            self, text="New", background="#E35FAF", command=self.enable_field)
         self.button_new.config(width=20, font=(
-            'arial', 10, 'bold'), fg='white')
-        self.button_new.grid(row=4, column=0)
+            'arial', 10, 'bold'), fg='#FFFFFF', cursor='hand2', activebackground='#692D51')
+        self.button_new.grid(row=4, column=0, padx=10, pady=10)
+
+        self.button_save = tk.Button(self, text="Save", background="#00C9F7", command=self.save_field)
+        self.button_save.config(width=20, font=(
+            'arial', 10, 'bold'), fg='#FFFFFF', cursor='hand2', activebackground='#002D37')
+        self.button_save.grid(row=4, column=1, padx=10, pady=10)
+
+        self.button_delete = tk.Button(
+            self, text="Delete", background="#F7004A", command=self.disable_field)
+        self.button_delete.config(width=20, font=(
+            'arial', 10, 'bold'), fg='#FFFFFF', cursor='hand2', activebackground='#500010')
+        self.button_delete.grid(row=4, column=2, padx=10, pady=10)
+
+    def enable_field(self):
+
+        self.my_name.set('')
+        self.my_duration.set('')
+        self.my_gender.set('')
+
+        self.entry_name.config(state='normal')
+        self.entry_duration.config(state='normal')
+        self.entry_gender.config(state='normal')
+
+        self.button_save.config(state='normal')
+        self.button_delete.config(state='normal')
+
+    def disable_field(self):
+        self.my_name.set('')
+        self.my_duration.set('')
+        self.my_gender.set('')
+
+        self.entry_name.config(state='disabled')
+        self.entry_duration.config(state='disabled')
+        self.entry_gender.config(state='disabled')
+
+        self.button_save.config(state='disabled')
+        self.button_delete.config(state='disabled')
+
+    def save_field(self):
+        
+        self.disable_field()
